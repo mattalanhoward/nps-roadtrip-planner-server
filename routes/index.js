feacafbdefb0e613c6e1b8a8f7774fa8776bc462
Matthew Howard <mattalanhoward@gmail.com>
2020-12-10 21:35:03 +0100

    api testing

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.post("/", function (req, res, next) {
   res.status(200).json({
-    npsApiToken: process.env.REACT_APP_NPS_API_KEY,
-    mapBoxApiToken: process.env.REACT_APP_MAPBOX_API_KEY,
+    npsApiToken: process.env.NPS_API_KEY,
+    mapBoxApiToken: process.env.MAPBOX_API_KEY,
   });
 });
 
