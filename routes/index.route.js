8cf1383d68420af520c1fab3a40181c5aacb6bba
Matthew Howard <mattalanhoward@gmail.com>
2021-01-02 21:08:47 +0100

    clean up

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,20 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res) => {
-  console.log(`GET IT`);
-  axios
-    .create({
-      baseURL: "https://developer.nps.gov/api/v1/",
-      headers: { "X-Api-Key": API_KEY },
-    })
-    .get(`/parks?limit=497`)
-    //return state info
-    // .then((response) => res.json(response.data))
-    .then((response) => console.log(response.data))
+/* GET all state parks from api. */
+// router.get("/", (req, res) => {
+//   console.log(" GET NPS API requested ");
+//   //call api
+//   axios
 
-    .catch((error) => error);
-});
+//     //filter by state
+//     .get(`/parks?statecode=${req.params.stateAbbr}`)
+//     //return state info
+//     .then((response) => console.log(response.data))
+
+//     // .then((response) => res.json(response.data))
+//     .catch((error) => error);
+// });
 
 module.exports = router;
