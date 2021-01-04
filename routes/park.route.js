d6e4611278f9736c8c9aff634f9b596488798335
Matthew Howard <mattalanhoward@gmail.com>
2021-01-04 13:15:46 +0100

    favorites working on front end

--- routes/park.route.js
+++ routes/park.route.js
@@ -7,22 +7,29 @@ const UserModel = require("../models/User.model");
 const API_KEY = process.env.NPS_API_KEY;
 // console.log(`Backend API`, API_KEY);
 
+/* POST get users favorite parks. */
+router.post("/usersFavorites", (req, res) => {
+  console.log(`Get favorite parks triggered`);
+  const { userId } = req.body;
+  console.log(`USER id`, userId);
+  UserModel.findOne({ _id: userId })
+    .then((user) => {
+      console.log(`Find user`, user.favoriteParks);
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      console.log(`Error getting user's favorites`, error);
+      res.status(500).json(error);
+    });
+});
+
 /* POST add favorite park. */
 router.post("/favorites", (req, res) => {
   const { parkCode, userId } = req.body;
   console.log(`Favorite Params `, parkCode);
   console.log(`Favorite UserId `, userId);
 
-  UserModel.findOne({ _id: userId })
-    .then((user) => {
-      user.favoriteParks.includes(parkCode)
-        ? user.favoriteParks.splice(user.favoriteParks.indexOf(parkCode), 1)
-        : user.favoriteParks.push(parkCode);
-
-      user.save();
-    })
-    .catch((error) => (`Error updating users favorites`, error));
-
+  //Add user to Park Model favorites.
   ParksModel.findOne({ parkCode: parkCode }, {}, { upsert: true })
     .then((park) => {
       if (park) {
@@ -37,8 +44,25 @@ router.post("/favorites", (req, res) => {
         });
       }
     })
+    .catch((error) => {
+      console.log(`Error updating park's user's favorites`, error);
+      res.status(500).json(error);
+    });
 
-    .catch((error) => (`error updating users favorites in parks`, error));
+  //Add favorite to User model.
+  UserModel.findOne({ _id: userId })
+    .then((user) => {
+      user.favoriteParks.includes(parkCode)
+        ? user.favoriteParks.splice(user.favoriteParks.indexOf(parkCode), 1)
+        : user.favoriteParks.push(parkCode);
+
+      user.save();
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      console.log(`Error user's favorites`, error);
+      res.status(500).json(error);
+    });
 });
 
 module.exports = router;
